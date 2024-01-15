74b4288dd9bdbfdd14bb9c38936d17219029d431
Chowdhury <sc26s@login.missouristate.edu>
2024-01-14 20:41:18 -0600

    variable java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 class Hello {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = a + b;
         System.out.println("Hello world");
+        System.out.println(c);
     }
 }
