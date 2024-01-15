5e085f9b48c61d6612a43103e2bffd7bc56e14a4
Chowdhury <sc26s@login.missouristate.edu>
2024-01-14 23:56:29 -0600

    type conversion

---
+++ variable.java
@@ -0,0 +1,11 @@
+class variable {
+    public static void main(String[] args) {
+        byte a = 127;
+        int b = 1244;
+        byte k =(byte)b;
+        float f= 5.6f;
+        int t=(int)f;
+        System.out.println(t);
+    }
+
+}
